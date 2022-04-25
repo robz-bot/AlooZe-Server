e19b6b552b55be922e13e87cf412ed02daca614e
robinpromantus2022 <robinsiva1998@gmail.com>
2022-04-25 21:59:06 +0530

    hello world added

--- app.js
+++ app.js
@@ -11,20 +11,32 @@ const pdfFile = fs.readFileSync(filepath);
 var server = express();
 
 var result = {};
-pdfparse(pdfFile).then(function(data) {
-    server.get("/data", function(req, res) {
-        console.log(data.numpages);
-        result = {
-            numPages: data.numpages,
-            info: data.numpinfoages,
-            content: data.text,
-        };
-        res.send(result);
-    });
+// pdfparse(pdfFile).then(function(data) {
+//     server.get("/data", function(req, res) {
+//         console.log(data.numpages);
+//         result = {
+//             numPages: data.numpages,
+//             info: data.numpinfoages,
+//             content: data.text,
+//         };
+//         res.send(result);
+//     });
 
-    console.log(result);
-    console.log("Stream ended...");
+//     console.log(result);
+//     console.log("Stream ended...");
+// });
+server.get("/hello", async(req, res) => {
+    try {
+        res.json({
+            status: 200,
+            message: "Hello World",
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500);
+    }
 });
+
 server.listen(port);
 // server.listen(port, hostname, () => {
 //     console.log(`Server running at http://${hostname}:${port}/`);
