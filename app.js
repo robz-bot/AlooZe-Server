6ce1b38730c7540c67ea2048d4d502df9dca8f45
robinpromantus2022 <robinsiva1998@gmail.com>
2022-04-25 22:34:33 +0530

    pdf logic added

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 const express = require("express");
-// const fs = require("fs");
-// const pdfparse = require("pdf-parse");
-
-// const hostname = "https://alooze-server.vercel.app/"; //"127.0.0.1"; // ||
+const fs = require("fs");
+const pdfparse = require("pdf-parse");
 
 const filepath = "demo.pdf";
-// const pdfFile = fs.readFileSync(filepath);
+const pdfFile = fs.readFileSync(filepath);
 
 const server = express();
 server.use(express.json({ extended: false }));
 
-// var result = {};
-// pdfparse(pdfFile).then(function(data) {
-//     server.get("/data", function(req, res) {
-//         console.log(data.numpages);
-//         result = {
-//             numPages: data.numpages,
-//             info: data.numpinfoages,
-//             content: data.text,
-//         };
-//         res.send(result);
-//     });
+var result = {};
+pdfparse(pdfFile).then(function(data) {
+    server.get("/data", function(req, res) {
+        console.log(data.numpages);
+        result = {
+            numPages: data.numpages,
+            info: data.numpinfoages,
+            content: data.text,
+        };
+        res.send(result);
+    });
 
-//     console.log(result);
-//     console.log("Stream ended...");
-// });
+    console.log(result);
+    console.log("Stream ended...");
+});
 server.get("/", async(req, res) => {
     try {
         res.json({
@@ -38,6 +36,3 @@ server.get("/", async(req, res) => {
 });
 const port = process.env.PORT || 8088;
 server.listen(port, () => console.log(`${port}`));
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
