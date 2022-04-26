d08933693a444887b6afa06b61e83ff3219bdb6a
robinpromantus2022 <robinsiva1998@gmail.com>
2022-04-26 12:30:31 +0530

    hello

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 const pdfContent = require("./api/get-pdf");
 
-app.use(express.json({ extended: false }));
+// app.use(express.json({ extended: false }));
 
 app.use("/api/pdf", pdfContent);
+app.use("/", (req, res) => {
+    res.json({
+        status: 200,
+        content: "hello world",
+    });
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
